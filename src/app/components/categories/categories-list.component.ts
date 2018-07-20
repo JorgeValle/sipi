d0341fa55e099e22825aca41075e48bf70d473da
Jorge Valle <jvalle@veic.org>
2018-07-19 22:45:31 -0400

    Some work, which will need further work, on homepage.

--- src/app/components/categories/categories-list.component.ts
+++ src/app/components/categories/categories-list.component.ts
@@ -4,29 +4,32 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
 
 // Sipi custom
-import { Category }        from './category';
-import { CategoryService } from '../../services/category.service';
+import { Category }           from './category';
+import { CategoryService }    from '../../services/category.service';
+import { SubcategoryService } from '../../services/subcategory.service';
 
 @Component({
   selector: 'categories-list',
   templateUrl: './categories-list.component.html',
-  providers: [CategoryService]
+  providers: [
+    CategoryService,
+    SubcategoryService
+  ]
 })
 
-// component classes manage the html templates
-// a component controls a patch of screen called a view
-// the class is where we define what the component does to support the view
 export class CategoriesListComponent implements OnInit {
 
   categories: Category[];
   category: Category;
 
+  subcategories;
+
   constructor(
     private categoryService: CategoryService,
+    private subcategoryService: SubcategoryService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
-
 
   /**
    * Gets all categories, via service
@@ -36,11 +39,26 @@ export class CategoriesListComponent implements OnInit {
     .getCategories()
     .subscribe(
       data => {
-        this.categories = data
+
+        this.categories = data;
+        
+        this.categories.map(x => {
+          this.subcategoryService.getSubcategories(x.system.id).subscribe(
+            data => {
+
+              let subcategories = data;
+
+            }
+          )
+        });
+
       }
     );
   }
 
+  /**
+   * 
+   */
   ngOnInit(): void {
     this.getCategories();
   }
